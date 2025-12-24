2acd5b040d63a7060b6ef321e79724100f0c76c8
SPCMSK <izaurietamatiasignacio@gmail.com>
2025-12-24 13:29:20 -0300

    fix: add missing link import

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useState, useEffect } from 'react';
+import Link from 'next/link';
 import { Sale } from '@/types';
 import TicketForm from '@/components/TicketForm';
 import SalesList from '@/components/SalesList';
